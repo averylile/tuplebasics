ddec9d7e803028cdfb086856bf010781da1a7835
Avery Lile <lileave000@warren.k12.in.us>
2024-11-11 10:03:48 -0500

    initial

---
+++ main.py
@@ -0,0 +1,26 @@
+color_list = ["red", "green", "blue", "yellow"]
+print(color_list[0])
+print(color_list[-1])
+slice_list = color_list[1:3]
+print(slice_list)
+
+
+
+
+
+num_list = [4, 6, 4, 2, 9, 4, 1]
+num_list.append(5)
+print(num_list)
+num_list.remove(4)
+print(num_list)
+print(num_list.count(4))
+print(num_list.index(9))
+
+
+
+
+fruit_list = ["apple", "banana", "cherry"]
+fruit_list[1] = "orange"
+print(fruit_list)
+fruit_list.pop()
+print(fruit_list)
